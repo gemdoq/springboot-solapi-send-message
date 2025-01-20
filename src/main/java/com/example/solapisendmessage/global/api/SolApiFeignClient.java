64940587fc9fde2c2dda4fbc4a2c4f4c33292faa
SeungHo <gemdoq@gmail.com>
2025-01-20 13:10:43 +0900

    feat(solapi): create feign client interface for API request

--- src/main/java/com/example/solapisendmessage/global/api/SolApiFeignClient.java
+++ src/main/java/com/example/solapisendmessage/global/api/SolApiFeignClient.java
@@ -1,14 +1,13 @@
 package com.example.solapisendmessage.global.api;
 
 import com.example.solapisendmessage.global.config.SolApiFeignConfig;
+import com.example.solapisendmessage.global.dto.SMSMessage;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-import java.util.Map;
-
 @FeignClient(name = "solapi", url = "${solapi.api.url}", configuration = SolApiFeignConfig.class)
 public interface SolApiFeignClient {
-	@PostMapping("/messages/v4/send")
-	String sendMessage(@RequestBody Map<String, Object> request);
+	@PostMapping(value = "/messages/v4/send-many/detail", consumes = "application/json")
+	String sendMessage(@RequestBody SMSMessage smsMessage);
 }
