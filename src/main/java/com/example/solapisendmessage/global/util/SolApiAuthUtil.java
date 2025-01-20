b27511d3543e7d5e393b4b237e69d8a0112fe7b1
SeungHo <gemdoq@gmail.com>
2025-01-20 13:09:43 +0900

    feat(solapi): create util for creating authorization headers for API requests

--- src/main/java/com/example/solapisendmessage/global/util/SolApiAuthUtil.java
+++ src/main/java/com/example/solapisendmessage/global/util/SolApiAuthUtil.java
@@ -1,12 +1,14 @@
 package com.example.solapisendmessage.global.util;
 
+import org.apache.commons.codec.binary.Hex;
+
 import javax.crypto.Mac;
 import javax.crypto.spec.SecretKeySpec;
 import java.nio.charset.StandardCharsets;
-import java.security.SecureRandom;
 import java.time.Instant;
+import java.time.ZoneOffset;
 import java.time.format.DateTimeFormatter;
-import java.util.Base64;
+import java.util.UUID;
 
 public class SolApiAuthUtil {
 
@@ -15,20 +17,20 @@ public class SolApiAuthUtil {
 			String data = dateTime + salt;
 			Mac mac = Mac.getInstance("HmacSHA256");
 			mac.init(new SecretKeySpec(apiSecret.getBytes(StandardCharsets.UTF_8), "HmacSHA256"));
-			byte[] hash = mac.doFinal(data.getBytes(StandardCharsets.UTF_8));
-			return Base64.getEncoder().encodeToString(hash);
+			String signature = new String(Hex.encodeHex(mac.doFinal(data.getBytes(StandardCharsets.UTF_8))));
+			return signature;
 		} catch (Exception e) {
 			throw new RuntimeException("signature 생성 에러", e);
 		}
 	}
 
 	public static String generateSalt() {
-		byte[] salt = new byte[16];
-		new SecureRandom().nextBytes(salt);
-		return Base64.getEncoder().encodeToString(salt);
+		return UUID.randomUUID().toString().replaceAll("-", "");
 	}
 
 	public static String getCurrentDateTime() {
-		return DateTimeFormatter.ISO_INSTANT.format(Instant.now());
+		return DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss'Z'")
+				.withZone(ZoneOffset.UTC)
+				.format(Instant.now());
 	}
 }
