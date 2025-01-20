d2dec0bdce45fb6996b0030bfa95f9873756dbbe
SeungHo <gemdoq@gmail.com>
2025-01-20 13:10:03 +0900

    feat(solapi): create config for creating authorization headers for API requests

--- src/main/java/com/example/solapisendmessage/global/config/SolApiFeignConfig.java
+++ src/main/java/com/example/solapisendmessage/global/config/SolApiFeignConfig.java
@@ -24,6 +24,11 @@ public class SolApiFeignConfig {
 			String salt = SolApiAuthUtil.generateSalt();
 			String signature = SolApiAuthUtil.generateSignature(apiSecret, dateTime, salt);
 
+			/**
+			 * API 레퍼런스의 예시 header 형식
+			 * `Authorization: <AuthenticationMethod> apiKey=<API Key>, date=<Date Time>, salt=<Salt>, signature=<Signature>`
+			 * header "Authorization: HMAC-SHA256 apiKey=NCSAYU7YDBXYORXC, date=2019-07-01T00:41:48Z, salt=jqsba2jxjnrjor, signature=1779eac71a24cbeeadfa7263cb84b7ea0af1714f5c0270aa30ffd34600e363b4"
+ 			 */
 			String authorizationHeader = String.format(
 					"HMAC-SHA256 apiKey=%s, date=%s, salt=%s, signature=%s",
 					apiKey, dateTime, salt, signature
