e38c4c401ea6a9f057f546e5b1cef8fb9a857b97
SeungHo <gemdoq@gmail.com>
2025-01-20 13:14:16 +0900

    feat(solapi): create a SolApiService for using the message sending API with Feign Client

--- src/main/java/com/example/solapisendmessage/global/service/SolApiService.java
+++ src/main/java/com/example/solapisendmessage/global/service/SolApiService.java
@@ -1,12 +1,18 @@
 package com.example.solapisendmessage.global.service;
 
 import com.example.solapisendmessage.global.api.SolApiFeignClient;
+import com.example.solapisendmessage.global.dto.SMSMessage;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import java.util.Map;
+import java.util.Collections;
+import java.util.List;
 
 @Service
 public class SolApiService {
+
+	@Value("${SOLAPI_API_MESSAGE_FROM}")
+	private String from;
 
 	private final SolApiFeignClient solApiFeignClient;
 
@@ -14,7 +20,12 @@ public class SolApiService {
 		this.solApiFeignClient = solApiFeignClient;
 	}
 
-	public String sendMessage(Map<String, Object> messageRequest) {
-		return solApiFeignClient.sendMessage(messageRequest);
+	public String sendSmsMessage(String toPhoneNumber, String textMessage) {
+		SMSMessage.Message message = new SMSMessage.Message(toPhoneNumber, from, textMessage);
+		List<SMSMessage.Message> messages = Collections.singletonList(message);
+
+		SMSMessage smsMessage = new SMSMessage(messages);
+
+		return solApiFeignClient.sendMessage(smsMessage);
 	}
 }
